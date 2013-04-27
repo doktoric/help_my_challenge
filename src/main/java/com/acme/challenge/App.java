a95eb8621a6dbee52ab75a4dea11f0170d34be43
Richard Doktorics <doktoricsrichard@gmail.com>
2013-04-28 00:35:55 +0200

    @martonsereg changes under maven

---
+++ src/main/java/com/acme/challenge/App.java
@@ -0,0 +1,29 @@
+package com.acme.challenge;
+
+import java.io.IOException;
+
+import jxl.write.WriteException;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws WriteException
+	 */
+	public static void main(String[] args) throws WriteException, IOException {
+		SimulatedMachineManager machineManager = new SimulatedMachineManager();
+		LogParser parser = new LogParser(machineManager);
+		// parser.start("d:\\prj\\help_my_challenge\\week_1.log");
+		parser.start("d:\\prj\\help_my_challenge\\week_1.log");
+		new ExcelExporter().exportMapsToExcel(
+				machineManager.getUrlStatistics(),
+				machineManager.getGeneralStatistics(),
+				machineManager.getExportStatistics());
+	}
+
+}
