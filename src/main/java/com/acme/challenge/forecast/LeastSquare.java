40e97cccf3faf1c28f3671e0e74596894f8c65c6
Richard Doktorics <doktoricsrichard@gmail.com>
2013-05-04 22:41:31 +0200

    forecast calculator

--- src/main/java/com/acme/challenge/forecast/LeastSquare.java
+++ src/main/java/com/acme/challenge/forecast/LeastSquare.java
@@ -1,7 +1,36 @@
 package com.acme.challenge.forecast;
 
+import java.util.PriorityQueue;
+
+import org.apache.commons.math3.stat.regression.RegressionResults;
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+
+import com.acme.challenge.UsageStatistics;
+
 public class LeastSquare {
 
+	public static double getSlope(PriorityQueue<UsageStatistics> statsQueue) {
+		double result = 0;
+		SimpleRegression regression = new SimpleRegression();
+		int i = 0;
+		for (UsageStatistics usageStatistics : statsQueue) {
+			regression.addData(i, usageStatistics.getUsedVMs());
+		}
+		result = regression.getSlope();
+		return result;
+
+	}
 	
+	public static double getIntercept(PriorityQueue<UsageStatistics> statsQueue) {
+		double result = 0;
+		SimpleRegression regression = new SimpleRegression();
+		int i = 0;
+		for (UsageStatistics usageStatistics : statsQueue) {
+			regression.addData(i, usageStatistics.getUsedVMs());
+		}
+		result = regression.getIntercept();
+		return result;
+
+	}
 
 }
